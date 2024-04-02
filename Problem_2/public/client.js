4933c049d89329bdda18c8e001aa3f95f9d53587
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2024-04-02 12:34:27 +0530

    push data

---
+++ Problem_2/public/client.js
@@ -0,0 +1,62 @@
+const socket = io()
+let name;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+do {
+    name = prompt('Please enter your name: ')
+} while(!name)
+
+function getmultiline() {
+    let lines = textarea.value.split('\n');
+    let result = '';
+    for (let i = 0; i < lines.length; i++) {
+        result += lines[i] + '<br>';
+    }
+    return result;
+}
+
+function send(){
+    let msg = textarea.value;
+    sendMessage(getmultiline());
+}
+
+function sendMessage(message) {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    // Append 
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    // Send to server 
+    socket.emit('message', msg)
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
+
+
+
